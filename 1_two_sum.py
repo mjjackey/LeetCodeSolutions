1e7e066659f46a9287d32c46af5bbb0eac1bc4aa
Jackey <mjjackey@163.com>
2025-05-15 16:45:29 +0800

    Amend some grammars

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,46 +1,49 @@
 """
 https://leetcode-cn.com/problems/two-sum/submissions/
 """
+from typing import Any
+
+
 class Solution(object):
-    def twoSum(nums, target):
+    def twoSum(numlist:list, target: int) -> list:
         """
-        :type nums: List[int]
+        :type numlist: List[int]
         :type target: int
         :rtype: List[int]
         """
         r = []
-        for i in range(len(nums)):
+        for i in range(len(numlist)):
             try:
-                if(i in r): continue
-                ind = nums.index(target-nums[i])
-                if(i==ind):continue  # for the element number is half of target
+                if i in r: continue
+                ind = numlist.index(target - numlist[i])
+                if i==ind :continue  # for the element number is half of target
             except:
                 continue
             else:
                 r.extend([i,ind])
         return r
 
-    def twoSum2(nums, target):
+    def twoSum2(numlist:list, target:int)->list:
         """
-        :type nums: List[int]
+        :type numlist: List[int]
         :type target: int
         :rtype: List[int]
         """
         nums_dict={}  # value:index in array, sum-value is not kept in the dictionary
         r=[]
-        for i in range(len(nums)):
-            if(nums_dict.get(target-nums[i])==None):
-                nums_dict.update({nums[i]: i})
+        for i in range(len(numlist)):
+            if nums_dict.get(target - numlist[i]) is None:
+                nums_dict.update({numlist[i]: i})
             else:
-                r.append(nums_dict[target-nums[i]])
+                r.append(nums_dict[target - numlist[i]])
                 r.append(i)
         return r
 
+if __name__=="__main__":
+    nums=[2,7,11,15]
+    r=Solution.twoSum(nums,9)
+    print(r)
 
-nums=[2,7,11,15]
-r=Solution.twoSum(nums,9)
-print(r)
-
-r=Solution.twoSum2(nums,9)
-print(r)
+    r=Solution.twoSum2(nums,9)
+    print(r)
 
